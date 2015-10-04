553dd6465c203f8e0d1843fa0cba557e64d18ee3
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2015-10-04 01:13:43 +0100

    Fix bug#23

--- Person.php
+++ Person.php
@@ -3,34 +3,9 @@
 class Person {
 
 	protected $name;
-	protected $age;
 
-
-	public function __construct($name, $age){
+	public function __construct($name){
 			$this->name = $name;
-			$this->age = $age;	
 	}
 
-
-    /**
-     * Gets the value of name.
-     *
-     * @return mixed
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * Sets the value of name.
-     *
-     * @param mixed $name the name
-     *
-     * @return self
-     */
-    protected function setName($name)
-    {
-        return $this->name = $name;
-    }
 }
